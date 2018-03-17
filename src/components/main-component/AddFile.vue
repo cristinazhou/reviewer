f654ef72d6d7ed4ad904e43911e1c6bb1a8747cc
leegendlee <731925534@qq.com>
2018-03-17 16:33:22 +0800

    [老峻峻]addFile

--- src/components/main-component/AddFile.vue
+++ src/components/main-component/AddFile.vue
@@ -1,30 +1,29 @@
 <template>
-  <Upload action="/file/upload" headers="">
-    <div style="padding: 20px 0">
-      <Icon type="ios-cloud-upload" size="60" style="color: #3399ff"></Icon>
-      <p>Click or drag files here to upload</p>
-    </div>
-  </Upload>
+    <Upload action="http://localhost:8080/file/upload">
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="60" style="color: #3399ff"></Icon>
+            <p>Click or drag files here to upload</p>
+        </div>
+    </Upload>
 </template>
 
 <script>
- export default {
-   data(){
-     return {
-     }
-   },
-   computed:{
-     file:function(){
-       return document.getElementsByTagName("Upload").name;
-     },
-     token:function(){
-       return localStorage.getItem("token");
-     }
-
-   },
-   methods:{
-   }
- };
+  export default {
+    data(){
+      return {
+        token: localStorage.getItem('token'),
+      }
+    },
+    computed: {
+      file: function () {
+        return document.getElementsByTagName("Upload").name;
+      },
+    },
+    methods: {},
+    created() {
+      document.cookie = 'X-CSRF-TOKEN' + this.token;
+    }
+  };
 
 
 </script>
