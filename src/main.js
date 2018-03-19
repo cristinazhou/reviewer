0988266fd9908914e3b172798ce78a953f8b894e
leegendlee <731925534@qq.com>
2018-03-19 19:29:51 +0800

    [老峻峻]BugFIx

--- src/main.js
+++ src/main.js
@@ -5,20 +5,19 @@ import App from './App'
 import router from './router'
 import iView from 'iview'
 import Axios from 'axios'
-
 import 'iview/dist/styles/iview.css'    // 使用 CSS
 Vue.use(iView);
 var http = Axios.create({
   timeout: 8000,
   baseURL: 'http://localhost:8080/sce_reviewer',
-})
-http.defaults.headers.common['Token'] = localStorage.getItem('token');
-Vue.prototype.$axios = http
+});
+http.defaults.headers.common['X-Token'] = localStorage.getItem('token');
+Vue.prototype.$axios = http;
 
 Vue.config.productionTip = false
 router.beforeEach((to, from, next) => {
   if (to.path !== '/' && to.path !== '/register' && to.path !== '/login') {
-    token = localStorage.getItem("token");
+    let token = localStorage.getItem("token");
     if (token) {
       next();
     } else {
